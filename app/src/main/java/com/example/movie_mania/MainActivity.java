9248e7955a7184fe43c0a8231e6e37b95828af3a
Chandan Acharya <chandan.acharya@alba-golf.com>
2019-07-04 13:28:01 -0500

    Custom toolbar added

--- app/src/main/java/com/example/movie_mania/MainActivity.java
+++ app/src/main/java/com/example/movie_mania/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         moviesRepository = MoviesRepository.getInstance();
 
@@ -152,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
                 currentPage = page;
                 isFetchingMovies = false;
 
+                setTitle();
+
             }
 
             @Override
@@ -159,6 +165,20 @@ public class MainActivity extends AppCompatActivity {
                 showError();
             }
         });
+    }
+
+    private void setTitle() {
+        switch (sortBy) {
+            case MoviesRepository.POPULAR:
+                setTitle(getString(R.string.popular));
+                break;
+            case MoviesRepository.TOP_RATED:
+                setTitle(getString(R.string.top_rated));
+                break;
+            case MoviesRepository.UPCOMING:
+                setTitle(getString(R.string.upcoming));
+                break;
+        }
     }
 
     private void showError() {
